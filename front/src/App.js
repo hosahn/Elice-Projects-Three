272e3c564caa2ee404c69588c278f317a4cc0dd2
Naayeon <naaay097@gmail.com>
2022-06-01 14:38:30 +0900

    Add setting

---
+++ front/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { BroswerRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <p>12팀</p>
+    </div>
+  );
+}
+
+export default App;
