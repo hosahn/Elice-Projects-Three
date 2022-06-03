e8849db0cf564fdfd86938f8d0bdb3dbf20da543
Shin-GC <tlsrhkdcjs12@naver.com>
2022-06-03 17:14:08 +0900

    Modify Sentry 버전 업데이트

--- back/src/app.js
+++ back/src/app.js
@@ -6,19 +6,23 @@ import swaggerUi from "swagger-ui-express";
 import { specs } from "./config/swaggerDoc.js";
 import "./config/env.js";
 import * as Sentry from "@sentry/node";
+import { BrowserTracing } from "@sentry/tracing";
 import compression from "compression";
 import csurf from "csurf";
 import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+
 export const app = express();
 
 Sentry.init({
   dsn: process.env.DSN,
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
 });
 
 const csrfProtection = csurf({ cookie: true });
 
-const RateLimit = require("express-rate-limit");
-const limiter = new RateLimit({
+const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
   delayMs: 0, // disable delaying — full speed until the max limit is reached
