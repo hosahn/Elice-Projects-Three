bf47af12c9d9b1ff35022e453761e5baa22ec8ee
Naayeon <naaay097@gmail.com>
2022-06-04 12:25:59 +0900

    Add pages

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,18 @@
+import React, { useState, useEffect, useReducer } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import { BroswerRouter, Route, Routes } from 'react-router-dom';
+import * as Api from './api';
+import Introduction from './pages/Introduction';
+import UserMain from './pages/UserMain';
 
 function App() {
   return (
-    <div className="App">
-      <p>12팀</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Introduction />} />
+        <Route path="/userMain" element={<UserMain />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ front/src/pages/UserMain.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserMain = () => {
+  return (
+    <>
+      <p>사용자메인페이지</p>
+    </>
+  );
+};
+
+export default UserMain;
