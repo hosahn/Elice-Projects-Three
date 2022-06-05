34c1d2f594efae58e81962570404eace816a9c5b
Hosan_Lee <hosahn13@gmail.com>
2022-06-05 23:48:41 +0900

    Fix disjoined Object Strategy => option, verify

--- back/src/passport/googleStrategy.js
+++ back/src/passport/googleStrategy.js
@@ -3,20 +3,18 @@ import { Strategy } from "passport-google-oauth20";
 import passport from "passport";
 import "../config/env.js";
 
+const option = {
+  clientID:
+    "179133829251-7vr8utiaenff1sheqp8rgdlill3qa34d.apps.googleusercontent.com",
+  clientSecret: "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",
+  callbackURL: "http://localhost:5001/user/googlecomplete",
+  passReqToCallback: true,
+};
+
+const verify = (request, accessToken, refreshToken, profile, done) => {
+  console.log(profile);
+  return done(null, profile);
+};
 export const GoogleStrategy = () => {
-  passport.use(
-    new Strategy(
-      {
-        clientID:
-          "179133829251-7vr8utiaenff1sheqp8rgdlill3qa34d.apps.googleusercontent.com",
-        clientSecret: "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",
-        callbackURL: "http://localhost:5001/user/googlecomplete",
-        passReqToCallback: true,
-      },
-      (request, accessToken, refreshToken, profile, done) => {
-        console.log(profile);
-        return done(null, profile);
-      }
-    )
-  );
+  passport.use(new Strategy(option, verify));
 };
--- back/src/passport/localStrategy.js
+++ back/src/passport/localStrategy.js
@@ -3,21 +3,21 @@ import passport from "passport";
 import { Strategy } from "passport-local";
 import "../config/env.js";
 //config
+const verify = (username, password, done) => {
+  User.findOne({ username: username }, function (err, user) {
+    if (err) {
+      return done(err);
+    }
+    if (!user) {
+      return done(null, false);
+    }
+    if (!user.verifyPassword(password)) {
+      return done(null, false);
+    }
+    return done(null, user);
+  });
+};
+
 export const LocalStrategy = () => {
-  passport.use(
-    new Strategy(function (username, password, done) {
-      User.findOne({ username: username }, function (err, user) {
-        if (err) {
-          return done(err);
-        }
-        if (!user) {
-          return done(null, false);
-        }
-        if (!user.verifyPassword(password)) {
-          return done(null, false);
-        }
-        return done(null, user);
-      });
-    })
-  );
+  passport.use(new Strategy(verify));
 };
