1b03678e8eeb88779ca4f09dd2679ee84671c85c
Shin-GC <tlsrhkdcjs12@naver.com>
2022-06-13 17:22:13 +0900

    Add 에러 처리 수정 및 404 추가

--- back/src/routers/diaryRouter.js
+++ back/src/routers/diaryRouter.js
@@ -73,29 +73,23 @@ const diaryRouter = Router();
 diaryRouter.post(
   "/",
   [
-    body("userId")
+    body("userId", "현재 접속해 있는 유저의 ID 값이 들어가 있지 않습니다.")
       .exists({ checkFalsy: true })
-      .withMessage("현재 접속해 있는 유저의 ID 값이 들어가 있지 않습니다.")
       .bail(),
-    body("title")
+    body("title", "제목은 필수로 입력해야 합니다.")
       .exists()
-      .withMessage("제목은 필수로 입력해야 합니다.")
       .bail(),
-    body("text")
+    body("text", "일기 내용은 필수로 적어주셔야 합니다.")
       .exists()
-      .withMessage("일기 내용은 필수로 적어주셔야 합니다.")
       .bail(),
     validate,
   ],
   async (req, res, next) => {
     const data = req.body;
     const { userId } = data;
-    try {
-      if (await DiaryService.challengeCheck(userId)) {
-        await DiaryService.check(userId);
-      }
-    } catch (error) {
-      next(error);
+    if (await DiaryService.challengeCheck(userId))
+    {
+      await DiaryService.check(userId);
     }
     try {
       const body = await DiaryService.create(data);
