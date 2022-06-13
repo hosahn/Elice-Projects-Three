84e9f2106ed3cfb7718b0c7363f7b518ececcab2
Naayeon <naaay097@gmail.com>
2022-06-13 18:58:38 +0900

    Add 이미지 추가 / 폴더 삭제

--- front/src/assets/images.js
+++ front/src/assets/images.js
@@ -2,12 +2,14 @@ import Background from './image/Background.jpg';
 import Google from './image/Google.png';
 import Kakao from './image/Kakao.png';
 import Naver from './image/Naver.png';
+import FortuneCookie from './image/FortuneCookie.png';
 
 const images = {
   Background,
   Google,
   Kakao,
   Naver,
+  FortuneCookie,
 };
 
 export default images;
