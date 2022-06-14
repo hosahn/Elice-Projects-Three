cd331bad3e1d05c1ee5985403dbd573ec81186c3
[4기-1반] 신광천 <tlsrhkdcjs12@naver.com>
2022-06-14 09:52:44 +0000

    Revert "Modify Merge Conflict 해결"
    This reverts merge request !29

--- front/src/pages/challenge/ChallengeCard.js
+++
@@ -1,9 +0,0 @@
-import { ChallengeCardContainer } from '../../styles/ChallengeStyle';
-import { ExplainContext } from '../../styles/CommonStyle';
-
-// 챌린지 받아온 정보
-const ChallengeCard = () => {
-  return <ChallengeCardContainer>dkdsfd</ChallengeCardContainer>;
-};
-
-export default ChallengeCard;
