ffd306c83e3f7909c8ac92874cef2fe28fb587b1
Naayeon <naaay097@gmail.com>
2022-06-16 10:50:35 +0900

    Merge dev-back , FE/Challenge/NY

--- back/src/db/models/Diary.js
+++ back/src/db/models/Diary.js
@@ -10,6 +10,12 @@ export default class Diary {
   static async create(newDiary) {
     const createDiary = await prisma.diary.create({
       data: newDiary,
+      select: {
+        id: true,
+        title: true,
+        text: true,
+        tag: true,
+      },
     });
     return createDiary;
   }
@@ -33,7 +39,8 @@ export default class Diary {
 
   /**
    * - 일기 상세 조회 Model 함수
-   * @param {number} diary_id - 조회할 다이어리 ID
+   * @param {number} id - 조회할 다이어리 ID
+   * @returns {Promise<{id:number, user_id:number, text: string, title: string, tag: string, date: Date, view: number}>}
    */
   static async read(id) {
     const updatePosts = await prisma.diary.update({
@@ -50,21 +57,104 @@ export default class Diary {
       where: {
         id: +id,
       },
+      select: {
+        id: true,
+        title: true,
+        text: true,
+        tag: true,
+        date: true,
+        view: true,
+      },
     });
     return readDiary;
   }
 
   /**
    * - 일기 목록 조회 Model 함수
-   * @param {number} user_id - 다이어리 목록을 조회할 유저 ID
+   * @param {number} userId - 다이어리 목록을 조회할 유저 ID
+   * @returns {Array.Promise<{id:number, text: string, title: string, tag: string, date: Date, view: number}>}
    */
-  static async readList(user_id) {
+  static async readList(userId) {
     const diaryList = await prisma.diary.findMany({
       where: {
-        user_id: +user_id,
+        user_id: +userId,
         deleted: false,
+      },
+      select: {
+        id: true,
+        title: true,
+        text: true,
+        tag: true,
+        date: true,
+        view: true,
       },
     });
     return diaryList;
+  }
+
+  /**
+   * - 일기가 존재하는지 확인하는 함수
+   * @param {number} id - Diary의 고유 id
+   * @returns {Promise<{id:number, user_id:number, text: string, title: string, tag: string, date: Date, view: number}>}
+   */
+  static async find(id) {
+    const diary = await prisma.diary.findFirst({
+      where: {
+        id: +id,
+      },
+    });
+    return diary;
+  }
+
+  /**
+   * - 유저가 챌린지에 참여하고 있을 경우, 그날 글을 썼는지 체크하는 업데이트 함수
+   * @param {number} userId - diary를 작성한 user_id
+   */
+  static async check(userId) {
+    const check = await prisma.user_challenge.updateMany({
+      where: {
+        user_id: +userId,
+      },
+      data: {
+        is_broken: false,
+      },
+    });
+    return check;
+  }
+
+  /**
+   * - 유저가 현재 진행 중인 챌린지가 있는지 검사하는 함수
+   * @param {number} userId - diary를 작성한 user_id
+   */
+  static async challengeCheck(userId) {
+    const challenge = await prisma.user_challenge.findFirst({
+      where: {
+        user_id: +userId,
+      },
+    });
+    return challenge;
+  }
+
+  /**
+   * - 현재 존재하는 유저인지 확인
+   * @param {number} userId - diary를 작성한 user_id
+   */
+  static async userCheck(userId) {
+    const user = await prisma.users.findFirst({
+      where: {
+        id: +userId,
+      },
+    });
+    return user;
+  }
+
+  /**
+   * - 랜덤한 일기 3개를 반환합니다.
+   * @returns {Array.Promise<{id:number, text: string, title: string, tag: string, date: Date, view: number}>}
+   */
+  static async randomDiarys(userId) {
+    const diarys =
+      await prisma.$queryRaw`SELECT * FROM diary WHERE deleted=0 AND user_id=${userId} ORDER BY RAND() limit 3;`;
+    return diarys;
   }
 }
--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -2,13 +2,12 @@ import { Router } from 'express';
 import passport from 'passport';
 import { User } from '../db/models/User.js';
 const userRouter = Router();
-
 userRouter.get(
   '/googlecomplete',
   passport.authenticate('google'),
   (req, res) => {
     if (req.isAuthenticated()) {
-      res.redirect('http://localhost:3000/usermain');
+      res.redirect('user/main');
     } else {
       res.redirect('/user/failed');
     }
@@ -17,7 +16,7 @@ userRouter.get(
 
 userRouter.get('/navercomplete', passport.authenticate('naver'), (req, res) => {
   if (req.isAuthenticated()) {
-    res.redirect('http://localhost:3000/usermain');
+    res.redirect('user/main');
   } else {
     res.redirect('/user/failed');
   }
@@ -25,7 +24,7 @@ userRouter.get('/navercomplete', passport.authenticate('naver'), (req, res) => {
 
 userRouter.get('/kakaocomplete', passport.authenticate('kakao'), (req, res) => {
   if (req.isAuthenticated()) {
-    res.redirect('http://localhost:3000/usermain');
+    res.redirect('user/main');
   } else {
     res.redirect('/user/failed');
   }
@@ -34,7 +33,7 @@ userRouter.get('/kakaocomplete', passport.authenticate('kakao'), (req, res) => {
 userRouter.get('/localcomplete', (req, res) => {
   passport.authenticate('local');
   if (req.isAuthenticated()) {
-    res.redirect('http://localhost:3000/usermain');
+    res.redirect('/user/main');
   } else {
     res.redirect('/user/failed');
   }
@@ -55,7 +54,7 @@ userRouter.get('/logout', (req, res) => {
 //CallBack Url이 리다이렉트 하는 경로
 
 //성공이면 true
-userRouter.get('/success', (req, res) => {
+userRouter.get('/main', (req, res) => {
   if (req.isAuthenticated()) {
     res.send(true);
   } else {
