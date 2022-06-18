423dcd8d6ea4aa09e5a08d7c4881ffbd5ff59431
Naayeon <naaay097@gmail.com>
2022-06-18 19:41:05 +0900

    Modify merge 오류 해결

--- front/src/styles/GlobalStyle.js
+++ front/src/styles/GlobalStyle.js
@@ -23,14 +23,12 @@ body{
   position: relative;
 }
 
-<<<<<<< HEAD
 button{
   border: none;
   background-color: transparent;
+  cursor: pointer;
 }
 
-=======
->>>>>>> 58c2a4824bc8a0eed052fabc56445770385a19d7
 button:hover{
   cursor: pointer;
 }
