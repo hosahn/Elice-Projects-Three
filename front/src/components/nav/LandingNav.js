1c7cfcfe0365c833927295a4c42125648cab4ae6
Shin-GC <tlsrhkdcjs12@naver.com>
2022-06-18 19:53:29 +0900

    Modify Merge conflict 해결

---
+++ front/src/components/nav/LandingNav.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+const NavWrapper = styled.div`
+  padding-top: 20px;
+  padding-bottom: 20px;
+  display: flex;
+  justify-content: end;
+`;
+
+const NavBtn = styled.button`
+  font-family: 'MICEMyungjo';
+  color: white;
+  font-size: 20px;
+  margin-right: 40px;
+  :hover {
+    opacity: 0.7;
+  }
+`;
+
+const LandingNav = () => {
+  const navigate = useNavigate();
+  return (
+    <NavWrapper>
+      <NavBtn onClick={() => navigate('/register')}>회원가입</NavBtn>
+      <NavBtn onClick={() => navigate('/login')}>로그인</NavBtn>
+    </NavWrapper>
+  );
+};
+
+export default LandingNav;
