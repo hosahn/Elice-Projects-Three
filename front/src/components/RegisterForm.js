cb5f50014a83e432a41b75ccace5873a03bc3492
Yujin <ywtv3636@naver.com>
2022-06-21 14:39:21 +0900

    Modify:코드리뷰반영

--- front/src/components/RegisterForm.js
+++ front/src/components/RegisterForm.js
@@ -5,21 +5,6 @@ import * as Api from '../api';
 import styled from 'styled-components';
 import images from '../assets/images';
 import { LoginInput, LoginText } from '../styles/LoginStyle';
-import LandingNav from './nav/LandingNav';
-
-// const RegisterInput = styled.input`
-//   display: inline-flex;
-//   width: 30rem;
-//   height: 3rem;
-//   color: white;
-//   background: transparent;
-//   padding: 1rem;
-//   border: solid 1px #dbc7ff;
-//   border-radius: 1rem;
-//   outline: none;
-//   font-size: 1rem;
-//   cursor: text;
-// `;
 
 const RegisterContainer = styled.div`
   position: relative;
@@ -43,20 +28,13 @@ const RegisterContainer = styled.div`
   }
 `;
 
-// const RegisterLabel = styled.div`
-//   font-family: 'EliceDigitalBaeum';
-//   font-size: 1rem;
-//   line-height: 1.5rem;
-//   color: white;
-// `;
-
 function RegisterForm() {
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [name, setName] = useState('');
-  const [duplicate, setDuplicate] = useState('');
+  c;
 
   const validateEmail = email => {
     return email.toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
@@ -70,7 +48,7 @@ function RegisterForm() {
 
   const isNameValid = name.length >= 2;
 
-  const isEmailDuplicate = async (e) => {
+  const isEmailDuplicate = async e => {
     e.preventDefault();
 
     try {
@@ -90,14 +68,9 @@ function RegisterForm() {
       }
     }
   };
-  const isFormValid =
-    isEmailValid &&
-    isPasswordValid &&
-    isPasswordSame &&
-    isNameValid &&
-    isEmailDuplicate;
+  const isFormValid = isEmailValid && isPasswordValid && isPasswordSame && isNameValid && isEmailDuplicate;
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async e => {
     e.preventDefault();
 
     try {
@@ -114,78 +87,37 @@ function RegisterForm() {
   };
 
   return (
-    <RegisterContainer id="RC">
+    <RegisterContainer id='RC'>
       <div onSubmit={handleSubmit}>
         <div style={{ marginBottom: '30px' }}>
           <LoginText>이메일 주소</LoginText>
-          <LoginInput
-            type="email"
-            autoComplete="off"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+          <LoginInput type='email' autoComplete='off' value={email} onChange={e => setEmail(e.target.value)} />
 
           <Btn text={'중복체크'} type={'sub'} onClick={isEmailDuplicate} />
 
-          {!isEmailValid && (
-            <LoginText className="text-success">
-              이메일 형식이 올바르지 않습니다.
-            </LoginText>
-          )}
+          {!isEmailValid && <LoginText className='text-success'>이메일 형식이 올바르지 않습니다.</LoginText>}
         </div>
 
         <div style={{ marginBottom: '30px' }}>
           <LoginText>비밀번호</LoginText>
-          <LoginInput
-            type="password"
-            autoComplete="off"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {!isPasswordValid && (
-            <LoginText className="text-success">
-              비밀번호는 4글자 이상으로 설정해 주세요.
-            </LoginText>
-          )}
+          <LoginInput type='password' autoComplete='off' value={password} onChange={e => setPassword(e.target.value)} />
+          {!isPasswordValid && <LoginText className='text-success'>비밀번호는 4글자 이상으로 설정해 주세요.</LoginText>}
         </div>
 
         <div style={{ marginBottom: '30px' }}>
           <LoginText>비밀번호 재확인</LoginText>
-          <LoginInput
-            type="password"
-            autoComplete="off"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-          {!isPasswordSame && (
-            <LoginText className="text-success">
-              비밀번호가 일치하지 않습니다.
-            </LoginText>
-          )}
+          <LoginInput type='password' autoComplete='off' value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} />
+          {!isPasswordSame && <LoginText className='text-success'>비밀번호가 일치하지 않습니다.</LoginText>}
         </div>
 
         <div style={{ marginBottom: '30px' }}>
           <LoginText>닉네임</LoginText>
-          <LoginInput
-            type="text"
-            autoComplete="off"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          {!isNameValid && (
-            <LoginText className="text-success">
-              닉네임은 2글자 이상으로 설정해 주세요.
-            </LoginText>
-          )}
+          <LoginInput type='text' autoComplete='off' value={name} onChange={e => setName(e.target.value)} />
+          {!isNameValid && <LoginText className='text-success'>닉네임은 2글자 이상으로 설정해 주세요.</LoginText>}
         </div>
 
         <div style={{ textAlign: 'center' }}>
-          <Btn
-            text={'회원가입'}
-            type={'sub'}
-            disabled={!isFormValid}
-            onClick={handleSubmit}
-          />
+          <Btn text={'회원가입'} type={'sub'} disabled={!isFormValid} onClick={handleSubmit} />
         </div>
       </div>
     </RegisterContainer>
