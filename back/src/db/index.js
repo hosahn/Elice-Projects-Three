c57f2897545453bc5a1d7bb82e440522a8e3d492
Shin-GC <tlsrhkdcjs12@naver.com>
2022-06-23 17:46:57 +0900

    Delete 프론트 폴더를 제외한 파일 삭제

--- back/src/db/index.js
+++
@@ -1,6 +0,0 @@
-//파일 구조를 위한 txt입니다!
-import { User } from "./models/User.js";
-import { Calendar } from "./models/Calendar.js";
-import Diary from "./models/Diary.js";
-import Emotion from "./models/Emotion.js";
-export { User, Calendar, Diary, Emotion };
