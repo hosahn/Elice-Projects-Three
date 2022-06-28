ca93a9b325f892dcfc3e75eb9ab85254b8ab4bb5
Hosan_Lee <hosahn13@gmail.com>
2022-06-29 01:49:06 +0900

    FE/LandingPage 80%/HS

--- front/src/pages/Landing.js
+++ front/src/pages/Landing.js
@@ -4,7 +4,25 @@ import images from "../assets/images";
 import * as style from "../styles/LandingStyle";
 import LandingNav from "../components/nav/LandingNav";
 import MainFooter from "../components/footer";
+import DiaryGraph from "../components/graph/diaryGraph";
+import SocialGraph from "../components/graph/socialGraph";
+import Btn from "../components/Btn";
 
+const Penciltext = `하루끝의 일기를 작성해보세요.`;
+const Emotiontext = `오늘의 감정을 확인해보세요.`;
+const Sporttext = `나에게 맞는 활동을 추천받으세요.`;
+const socialText = `일기는 일기장에만?
+언제 어디서나 밤하늘에 접속하세요.
+33%의 사람들이 웹서비스를 이용해
+일기를 작성하고 있습니다.`;
+const snsText = `SNS에 피로감을 느끼시나요?
+여러분만 그런 게 아닙니다.
+시간이 흐르면 흐를수록
+SNS에 피로감을 느끼는 MZ세대가 
+증가하는 추세입니다.`;
+const quotation = `"당신의 내일을 밤하늘이 응원합니다."
+
+-엘리스 12팀 일동-`;
 const Landing = () => {
   return (
     <>
@@ -36,7 +54,34 @@ const Landing = () => {
           <style.MoonIcon />
         </style.LogoContainer>
         <style.WhyWe>서비스 밤하늘</style.WhyWe>
-        <style.SocietyProblem>해결하고자 하는 문제</style.SocietyProblem>
+        <style.PencilIcon />
+        <style.PencilText>{Penciltext}</style.PencilText>
+        <style.HeartIcon />
+        <style.EmotionText>{Emotiontext}</style.EmotionText>
+        <style.JustifyContainer>
+          <style.HeartEmotion HeartColor="#FFEC99" Top="40px" Left="70px" />
+          <style.HeartEmotion HeartColor="#A5D8FF" Top="160px" Left="70px" />
+          <style.HeartEmotion HeartColor="#FFD6A5" Top="280px" Left="70px" />
+          <style.HeartEmotion HeartColor="#CED4DA" Top="40px" Left="310px" />
+          <style.HeartEmotion HeartColor="#FFADAD" Top="160px" Left="310px" />
+          <style.HeartEmotion HeartColor="#BDB2FF" Top="280px" Left="310px" />
+          <style.HeartEmotion HeartColor="#D8F5A2" Top="160px" Left="550px" />
+        </style.JustifyContainer>
+        <style.SportIcon />
+        <style.SportText>{Sporttext}</style.SportText>
+        <style.ButtonDiv>
+          <Btn text={"일기 쓰기 바로가기"} />
+        </style.ButtonDiv>
+        <style.SocietyProblem>왜 밤하늘일까?</style.SocietyProblem>
+        <style.GraphContainer>
+          <DiaryGraph />
+          <style.REmotionText>{socialText}</style.REmotionText>
+          <SocialGraph /> <style.RSocialText>{snsText}</style.RSocialText>
+        </style.GraphContainer>
+        <style.ButtonDiv>
+          <Btn text={"밤하늘 서비스 바로가기"} />
+        </style.ButtonDiv>
+        <style.Quotation>{quotation}</style.Quotation>
       </style.MainContainer>
       <MainFooter></MainFooter>
     </>
--- front/src/styles/LandingStyle.js
+++ front/src/styles/LandingStyle.js
@@ -1,5 +1,123 @@
 import styled, { keyframes } from "styled-components";
 import { Moon } from "@styled-icons/heroicons-solid/Moon";
+import { PencilAlt } from "@styled-icons/fa-solid/PencilAlt";
+import { HeartFill } from "@styled-icons/bootstrap/HeartFill";
+import { MusicNoteList } from "@styled-icons/bootstrap/MusicNoteList";
+
+export const MoonIcon = styled(Moon)`
+  color: white;
+  width: 250px;
+  height: 250px;
+  top: 100px;
+  position: absolute;
+  left: 190px;
+  margin: auto;
+  transition: all 0.8s ease-in-out;
+  &:hover {
+    color: yellow;
+  }
+`;
+
+export const SportIcon = styled(MusicNoteList)`
+  color: white;
+  width: 100px;
+  height: 100px;
+  top: 850px;
+  position: relative;
+  left: 400px;
+  margin: auto;
+  transition: all 0.8s ease-in-out;
+  &:hover {
+    color: black;
+  }
+`;
+
+export const HeartIcon = styled(HeartFill)`
+  color: white;
+  width: 100px;
+  height: 100px;
+  top: 800px;
+  position: relative;
+  left: 400px;
+  margin: auto;
+  transition: all 0.8s ease-in-out;
+  &:hover {
+    color: red;
+  }
+`;
+
+export const ButtonDiv = styled.div`
+  position: relative;
+  margin: auto;
+  top: 910px;
+  width: 800px;
+  height: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  grid-auto-rows: auto;
+`;
+
+export const Quotation = styled.h1`
+  top: 1050px;
+  font-size: 32px;
+  font-family: "EliceDigitalBaeum_Bold";
+  color: black;
+  color: white;
+  text-align: center;
+  white-space: pre-wrap;
+`;
+
+export const PencilIcon = styled(PencilAlt)`
+  color: white;
+  width: 100px;
+  height: 100px;
+  top: 750px;
+  position: relative;
+  left: 400px;
+  margin: auto;
+  transition: all 0.8s ease-in-out;
+  &:hover {
+    color: black;
+  }
+`;
+
+export const PencilText = styled.h1`
+  font-family: "EliceDigitalBaeum_Bold";
+  font-size: 38px;
+  text-align: center;
+  color: white;
+  top: 680px;
+  left: 100px;
+  word-spacing: 10px;
+  line-height: 39px;
+  white-space: pre-wrap;
+`;
+
+export const SportText = styled.h1`
+  font-family: "EliceDigitalBaeum_Bold";
+  font-size: 38px;
+  text-align: center;
+  color: white;
+  top: 780px;
+  left: 100px;
+  word-spacing: 10px;
+  line-height: 39px;
+  white-space: pre-wrap;
+`;
+
+export const EmotionText = styled.h1`
+  font-family: "EliceDigitalBaeum_Bold";
+  font-size: 38px;
+  text-align: center;
+  color: white;
+  top: 725px;
+  left: 100px;
+  word-spacing: 10px;
+  line-height: 39px;
+  white-space: pre-wrap;
+`;
+
 export const ImageInput = styled.input`
   display: inline-flex;
   width: 30rem;
@@ -19,18 +137,29 @@ export const ImageInput = styled.input`
 
 export const JustifyContainer = styled.div`
   border-radius: 30px;
-  opacity: 0.5;
+  opacity: 0.7;
   position: relative;
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 800px;
-  margin-bottom: 60px;
-  width: 1200px;
-  height: 00px;
+  margin: auto;
+  top: 800px;
+  width: 800px;
+  height: 400px;
   background-color: white;
   display: flex;
   flex-direction: column;
   align-items: center;
+  grid-auto-rows: auto;
+`;
+
+export const GraphContainer = styled.div`
+  border-radius: 30px;
+  position: relative;
+  margin: auto;
+  top: 1200px;
+  width: 900px;
+  height: 900px;
+  display: flex;
+  flex-direction: column;
+  align-items: left;
 `;
 
 export const WhyWe = styled.h1`
@@ -42,23 +171,38 @@ export const WhyWe = styled.h1`
   word-spacing: 30px;
 `;
 
+export const HeartText = styled.h1`
+  font-family: "EliceDigitalBaeum_Bold";
+  font-size: 30px;
+  top : ${(props) => props.Top};
+  left : ${(props) => props.Left}
+  color: white;
+  word-spacing: 10px;
+`;
+
 export const SocietyProblem = styled.h1`
   font-family: "EliceDigitalBaeum_Bold";
   font-size: 50px;
   text-align: center;
   color: white;
-  top: 1200px;
+  top: 1100px;
   word-spacing: 30px;
 `;
 export const MainContainer = styled.div`
   position: relative;
-  height: 400vh;
+  height: 520vh;
   width: 100%;
   overflow: hidden;
   background-image: linear-gradient(to bottom, #09203f 0%, #537895 100%);
-  &::-webkit-scrollbar {
-    display: none;
-  }
+`;
+export const HeartEmotion = styled(HeartFill)`
+  width: 80px;
+  height: 80px;
+  color: ${(props) => props.HeartColor};
+  position: absolute;
+  top: ${(props) => props.Top};
+  left: ${(props) => props.Left};
+  margin: auto;
 `;
 
 export const LogoContainer = styled.div`
@@ -144,6 +288,36 @@ export const Note6 = styled.div`
   right: -22px;
 `;
 
+export const REmotionText = styled.h3`
+  position: absolute;
+  width: 630px;
+  height: 163px;
+  left: 500px;
+  top: 15px;
+  font-family: "EliceDigitalBaeum_Bold";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 28px;
+  line-height: 39px;
+  white-space: pre-wrap;
+  color: white;
+`;
+
+export const RSocialText = styled.h3`
+  position: absolute;
+  width: 630px;
+  height: 163px;
+  left: 500px;
+  top: 270px;
+  font-family: "EliceDigitalBaeum_Bold";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 28px;
+  line-height: 39px;
+  white-space: pre-wrap;
+  color: white;
+`;
+
 export const LineOne = styled.div`
   width: 750px;
   height: 4px;
@@ -177,16 +351,6 @@ export const LineFour = styled.div`
   background-color: white;
   position: absolute;
   top: 607px;
-`;
-
-export const MoonIcon = styled(Moon)`
-  color: white;
-  width: 250px;
-  height: 250px;
-  top: 100px;
-  position: absolute;
-  left: 190px;
-  margin: auto;
 `;
 
 const boxFade = keyframes`
@@ -228,7 +392,7 @@ export const StarFalling = styled.span`
     background: linear-gradient(90deg, #fff, transparent);
   }
 
-  &:first {
+  &:nth-child(1) {
     top: 10px;
     right: 0px;
     left: initial;
