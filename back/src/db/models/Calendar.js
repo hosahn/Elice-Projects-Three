945bb20daa97fd2514228c7795204d34e07cff13
Hosan_Lee <hosahn13@gmail.com>
2022-06-30 03:09:45 +0900

    Merging

--- back/src/db/models/Calendar.js
+++ back/src/db/models/Calendar.js
@@ -4,22 +4,22 @@ const prisma = new PrismaClient();
 class Calendar {
   static async findMonthly({ fromDate, toDate, user_id }) {
     const result =
-      await prisma.$queryRaw`SELECT * FROM diary  WHERE user_id = ${user_id} AND (date between date_format(${fromDate}, '%Y-%m-%d/%H:%m:%s') and date_format(${toDate}, '%Y-%m-%d/%H:%m:%s'))`;
+      await prisma.$queryRaw`SELECT * FROM diary  WHERE user_id = ${user_id} AND (date between date_format(${fromDate}, '%Y-%m-%d/%H:%m:%s') and date_format(${toDate}, '%Y-%m-%d/%H:%m:%s')) AND deleted = 0`;
     return result;
   }
   static async findMonthlyEmotion({ fromDate, toDate, user_id }) {
     const result =
-      await prisma.$queryRaw`SELECT * FROM diary  WHERE user_id = ${user_id} AND (date between date_format(${fromDate}, '%Y-%m-%d/%H:%m:%s') and date_format(${toDate}, '%Y-%m-%d/%H:%m:%s'))`;
+      await prisma.$queryRaw`SELECT * FROM diary  WHERE user_id = ${user_id} AND (date between date_format(${fromDate}, '%Y-%m-%d/%H:%m:%s') and date_format(${toDate}, '%Y-%m-%d/%H:%m:%s')) AND deleted = 0`;
     return 0;
   }
   static async findMonthlyChallenge({ fromDate, toDate, user_id }) {
     const result =
-      await prisma.$queryRaw`SELECT * FROM diary  WHERE user_id = ${user_id} AND (date between date_format(${fromDate}, '%Y-%m-%d/%H:%m:%s') and date_format(${toDate}, '%Y-%m-%d/%H:%m:%s'))`;
+      await prisma.$queryRaw`SELECT * FROM diary  WHERE user_id = ${user_id} AND (date between date_format(${fromDate}, '%Y-%m-%d/%H:%m:%s') and date_format(${toDate}, '%Y-%m-%d/%H:%m:%s')) AND deleted = 0`;
     return 0;
   }
   static async findMonthlyViews({ fromDate, toDate, user_id }) {
     const result =
-      await prisma.$queryRaw`SELECT * FROM diary  WHERE user_id = ${user_id} AND (date between date_format(${fromDate}, '%Y-%m-%d/%H:%m:%s') and date_format(${toDate}, '%Y-%m-%d/%H:%m:%s'))`;
+      await prisma.$queryRaw`SELECT * FROM diary  WHERE user_id = ${user_id} AND (date between date_format(${fromDate}, '%Y-%m-%d/%H:%m:%s') and date_format(${toDate}, '%Y-%m-%d/%H:%m:%s')) AND deleted = 0`;
     return 0;
   }
 }
